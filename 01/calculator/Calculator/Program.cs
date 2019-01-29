b06f8dde6be616ef76a3704c20572b8ecb04c92b
Mikkel O. Laursen <m@ikkel.dk>
2019-01-29 11:34:56 +0100

    Add Exercise 1

---
+++ 01/calculator/Calculator/Program.cs
@@ -0,0 +1,89 @@
+﻿using System;
+
+namespace Calculator
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            Calculator Calc = new Calculator();
+
+            Console.WriteLine("Input two numbers to add: ");
+            double a = Convert.ToDouble(Console.ReadLine());
+            double b = Convert.ToDouble(Console.ReadLine());            
+
+            Console.WriteLine(
+                "Result: " + 
+                Calc.Add(a, b) +
+                "\n" +
+                "--------------" +
+                "\n"
+            );
+
+            Console.WriteLine("Input two numbers to subtract: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());            
+
+            Console.WriteLine(
+                "Result: " + 
+                Calc.Subtract(a, b) +
+                "\n" +
+                "--------------" +
+                "\n"
+            );
+
+            Console.WriteLine("Input two numbers to multiply: ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());            
+
+            Console.WriteLine(
+                "Result: " + 
+                Calc.Multiply(a, b) +
+                "\n" +
+                "--------------" +
+                "\n"
+            );
+
+            Console.WriteLine("Input two numbers to exponentialize (yes it's a word): ");
+            a = Convert.ToDouble(Console.ReadLine());
+            b = Convert.ToDouble(Console.ReadLine());            
+
+            Console.WriteLine(
+                "Result: " + 
+                Calc.Power(a, b) +
+                "\n" +
+                "--------------" +
+                "\n"
+            );
+        }
+
+        public double Add(double a, double b)
+        {
+            return a + b;
+        }
+
+        public double Subtract(double a, double b)
+        {
+            return a - b;
+        }
+
+        public double Multiply(double a, double b)
+        {
+            return a * b;
+        }
+
+        public double Power(double x, double exp)
+        {
+            if (exp < 1) return x;
+
+            double temp = 1;
+
+            while (exp > 0) {
+                temp *= x;
+                exp--;
+            }
+
+            return temp;
+        }
+    }
+}
