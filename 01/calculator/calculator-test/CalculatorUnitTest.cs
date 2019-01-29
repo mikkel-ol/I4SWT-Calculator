c32bcb2723264e7813ed7d590942cc4a42b8c1aa
Mikkel O. Laursen <m@ikkel.dk>
2019-01-29 13:55:06 +0100

    Update NUnit test cases

--- 01/calculator/calculator-test/CalculatorUnitTest.cs
+++ 01/calculator/calculator-test/CalculatorUnitTest.cs
@@ -13,10 +13,28 @@ namespace Tests
             Calc = new Calculator();
         }
 
-        [Test]
-        public void TestAdd1()
+        [TestCase(10, 20, 30)]
+        [TestCase(-2, -4, -6)]
+        [TestCase(-2, 4, 2)]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, 1)]
+        [TestCase(0, -1, -1)]
+        [TestCase(1.5, 2.4, 3.9)]
+        [TestCase(-1.15, 1.3, 0.15)]
+        public void TestAdd(double a, double b, double result)
         {
-            Assert.That(1000, Is.EqualTo(Calc.Add(1001, -1)));
+            Assert.That(result, Is.EqualTo(Calc.Add(a, b)).Within(.1));
+        }
+
+        [TestCase(10, 20, -10)]
+        [TestCase(-2, -4, 2)]
+        [TestCase(-2, 4, -6)]
+        [TestCase(0, 0, 0)]
+        [TestCase(0, 1, -1)]
+        [TestCase(0, -1, 1)]
+        public void TestSubtract(double a, double b, double result)
+        {
+            Assert.That(result, Is.EqualTo(Calc.Subtract(a, b)));
         }
     }
 }
