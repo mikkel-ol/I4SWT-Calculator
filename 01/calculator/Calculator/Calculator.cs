e6798cbd693e58a724118c7e9b1a63a285583d0f
Søren <soren9els1@users.noreply.github.com>
2019-01-30 11:48:34 +0100

    Add Calc Constructor()

--- 01/calculator/Calculator/Calculator.cs
+++ 01/calculator/Calculator/Calculator.cs
@@ -4,6 +4,8 @@ namespace Calculators
 {
     public class Calculator
     {
+        public Calculator() => Clear();
+
         static void Main(string[] args)
         {
             Calculator Calc = new Calculator();
