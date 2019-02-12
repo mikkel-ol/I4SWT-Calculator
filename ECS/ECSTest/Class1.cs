22dad9ac85f00992103cda807f1b51d0beafc37f
Søren <soren9els1@users.noreply.github.com>
2019-02-12 13:04:30 +0100

    Test NUnit attributes

--- ECS/ECSTest/Class1.cs
+++ ECS/ECSTest/Class1.cs
@@ -5,6 +5,7 @@ using ECSSWT;
 
 namespace ECS.Test
 {
+    [TestFixture]
     public class Class1
     {
         // Objects pref. interfaces
@@ -18,7 +19,20 @@ namespace ECS.Test
         }
 
 
-        []
+/* [Random(-1,2,5)]
+ * [Values(1,2,3,4,5,)]
+ * 
+        [TestCase(Values(1, 2, 3, 4),Random(-1, -2, 4), TestName = "bb")]
+        public void Test_Values_Random( double a, double b)
+        {
+            double result = a + b;
+            Assert.That(result, Is.EqualTo(a+b));
+        }
+        
+*/
+
+
+        
 
     }
 }
