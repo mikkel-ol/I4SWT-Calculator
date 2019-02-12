45792ff66ec1a9a203fc9c34877a7f6ccee34ec6
Alexander Bjerre <201305878@post.au.dk>
2019-02-12 14:59:58 +0100

    Add ECS and Heater

---
+++ ECS/ECS/ECS.cs
@@ -0,0 +1,46 @@
+﻿namespace ECS.Legacy
+{
+    public class ECS
+    {
+        private int _threshold;
+        private readonly TempSensor _tempSensor;
+        private readonly Heater _heater;
+
+        public ECS(int thr)
+        {
+            SetThreshold(thr);
+            _tempSensor = new TempSensor();
+            _heater = new Heater();
+        }
+
+        public void Regulate()
+        {
+            var t = _tempSensor.GetTemp();
+            if (t < _threshold)
+                _heater.TurnOn();
+            else
+                _heater.TurnOff();
+
+        }
+
+        public void SetThreshold(int thr)
+        {
+            _threshold = thr;
+        }
+
+        public int GetThreshold()
+        {
+            return _threshold;
+        }
+
+        public int GetCurTemp()
+        {
+            return _tempSensor.GetTemp();
+        }
+
+        public bool RunSelfTest()
+        {
+            return _tempSensor.RunSelfTest() && _heater.RunSelfTest();
+        }
+    }
+}
